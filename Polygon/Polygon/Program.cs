e7c08e6742b9760917a7d74ad819d36d3020fcd5
Andrew Stanek <andrews@milnerbrowne.com>
2017-02-12 22:11:13 +0100

    User menu and navigation.

--- Polygon/Polygon/Program.cs
+++ Polygon/Polygon/Program.cs
@@ -7,8 +7,30 @@ using Polygon.Models;
 
 namespace Polygon {
   class Program {
+    static int option = 0;
+    static int polygonPointCount = 0;
     static void Main(string[] args) {
-      
+      Console.WriteLine("Polygon surface area calculator.");
+
+      //Main loop of the program
+      while (option != 2) {
+        Console.WriteLine("Menu:");
+        Console.WriteLine("1. Calculate polygon surface");
+        Console.WriteLine("2. Exit");
+        option = Convert.ToInt32(Console.ReadLine());
+        Console.Clear();
+
+        switch (option) {
+          case 1:
+            break;
+          case 2:
+            break;
+          default:            
+            Console.WriteLine("Unknown operation.");
+            break;
+        }
+      }
+
     }
   }
 }
