add183ca3fd6cc33207e6f5ddfcdfc7998891d8f
Andrew Stanek <andrews@milnerbrowne.com>
2017-02-12 21:39:58 +0100

    Added two core classes for calculating polygon surface area

---
+++ Polygon/Polygon/Models/Point.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygon.Models {
+
+  /* Class defining point in cartesian coordinate system. */
+  class Point {
+    double x;
+    double y;
+  }
+}
---
+++ Polygon/Polygon/Models/Polygon2D.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygon.Models {
+  class Polygon2D {
+    private List<Point> points;
+
+    public Polygon2D(List<Point> points) {
+      this.points = points;
+    }
+  }
+}
